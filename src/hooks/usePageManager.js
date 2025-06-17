c791399cedf1e98c34477b1b1decc7ccd42196bb
Hugo Hebert <hugo.hebert00@gmail.com>
2025-06-17 10:46:10 +0200

    [BugFix] Fixed authentication user flow bug.
    Fixed bug where user had to login twice to get to app page.
    
    The issue was that first login wasn't taken into account, due to page
    manager not letting the time to spotify auth page to retrieve token info.
    To fix this, a sleep function has been added to pageManager when current
    route is spotify authentication.

--- src/hooks/usePageManager.js
+++ src/hooks/usePageManager.js
@@ -1,20 +1,39 @@
 import { useEffect } from 'react';
 import { useSpotifyTokenManager } from './useSpotifyTokenManager';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { ROUTES } from '../config/routes';
+import sleep from '../utils/utils';
 
 export const usePageManager = () => {
 
     const [spotifyToken, isSpotifyTokenValid] = useSpotifyTokenManager();
     const navigate = useNavigate();
+    const location = useLocation();
 
     useEffect(() => {
-        if (isSpotifyTokenValid()) {
-            navigate(ROUTES.APP);
-        } else {
-            navigate(ROUTES.HOME);
+        const changeRoute = async () => {
+            if (location.pathname === ROUTES.SPOTIFY_AUTH)
+            {
+                await sleep(1000); // Wait to let time to retrieve info from API on auth page
+            }
+
+            if (isSpotifyTokenValid()) {
+                navigate(ROUTES.APP);
+            } else {
+                navigate(ROUTES.HOME);
+            }
         }
+
+        changeRoute();
+       
     }, [spotifyToken]);
+
+    useEffect(() => {
+
+        const interval = setInterval(() => {console.log('spotify token is valid', isSpotifyTokenValid())}, 20*1000);
+
+        return () => {clearInterval(interval)};
+    }, [])
 
     return;
 }
